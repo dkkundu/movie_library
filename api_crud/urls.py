2b685fea9ce59b1e5f330fdeae5fbba95423bbe0
Dipto K Kundu <dkkundu00@gmail.com>
2022-11-02 17:56:48 +0600

    add home page

--- api_crud/urls.py
+++ api_crud/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
 from django.urls import include, path
+from authentication.web_view import Home
 
 # urls
 urlpatterns = [
+    path('', Home.as_view(), name="home"),
     path('api/v1/movies/', include('movies.urls')),
     path('api/v1/auth/', include('authentication.urls')),
     path('admin/', admin.site.urls),
