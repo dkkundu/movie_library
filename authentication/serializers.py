eebe3672473a86f2d500d9b93eb8d9ca53b72c77
Dipto K Kundu <dkkundu00@gmail.com>
2022-11-02 15:34:56 +0600

    Solved Token Generating error

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -15,7 +15,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         style={'input_type': 'password', 'placeholder': 'Password'}
     )
     password2 = serializers.CharField(
-        write_only=True, required=True,
+        write_only=True, required=True, validators=[validate_password],
         style={'input_type': 'password', 'placeholder': 'Password'}
     )
 
@@ -27,11 +27,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         )
 
     def validate(self, attrs):
-        if attrs['password'] == attrs['password2']:
-            raise serializers.ValidationError(
-                {"password": "Password fields didn't match."}
-            )
-        if attrs['password'] != attrs['password2']:
+        if attrs['password'] is attrs['password2']:
             raise serializers.ValidationError(
                 {"password": "Password fields didn't match."}
             )
@@ -40,8 +36,6 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         del validated_data['password2']
         user = User.objects.create(**validated_data)
-
         user.set_password(validated_data['password'])
         user.save()
-
         return user
